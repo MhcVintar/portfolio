3fdedb0769447b78c0dd5030345ccad6d2d728d3
mihav <mhckhc.vintar@gmail.com>
2024-02-06 16:50:38 +0100

    completed the section divider (color may still change)

--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -7,52 +7,70 @@ export default function SectionDivider() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="74.68"
-      height="151.13"
+      height="153.13"
       className="my-16"
     >
       <motion.line
-        x1="14.62"
+        x1="15.62"
         y1="2"
-        x2="14.62"
+        x2="15.62"
         y2="123.88"
-        stroke="black"
+        stroke="#fcd34d"
         strokeWidth="4"
         strokeLinecap="round"
         initial={{ opacity: 0, pathLength: 0 }}
         animate={{ opacity: 1, pathLength: 1 }}
         transition={{ duration: 0.2 }}
       />
-      <motion.circle
-        cx="14.62"
-        cy="136.51"
-        r="12.62"
-        stroke="black"
+      <motion.path
+        d="M 15.62 123.88 A 8 8 90 0 1 15.62 139.88"
+        stroke="#fcd34d"
         strokeWidth="4"
+        strokeLinecap="round"
         fill="none"
-        initial={{ pathLength: 0, rotate: -90 }}
-        animate={{ pathLength: 1, rotate: -90 }}
+        initial={{ opacity: 0, pathLength: 0 }}
+        animate={{ opacity: 1, pathLength: 1 }}
+        transition={{ delay: 0.2, duration: 0.2 }}
+      />
+      <motion.path
+        d="M 15.62 123.88 A 8 8 90 0 0 15.62 139.88"
+        stroke="#fcd34d"
+        strokeWidth="4"
+        strokeLinecap="round"
+        fill="none"
+        initial={{ opacity: 0, pathLength: 0 }}
+        animate={{ opacity: 1, pathLength: 1 }}
         transition={{ delay: 0.2, duration: 0.2 }}
       />
       <motion.polyline
-        points="51.81 2 51.81 51.3 60.06 81.73 60.06 111.26"
-        stroke="black"
+        points="50.81 2 50.81 51.3 59.06 81.73 59.06 111.26"
+        stroke="#fcd34d"
         strokeWidth="4"
         strokeLinecap="round"
-        strokeMiterlimit="10"
+        strokeMiterlimit="8"
         fill="none"
         initial={{ opacity: 0, pathLength: 0 }}
         animate={{ opacity: 1, pathLength: 1 }}
         transition={{ duration: 0.2 }}
       />
-      <motion.circle
-        cx="60.06"
-        cy="123.88"
-        r="12.62"
-        stroke="black"
+      <motion.path
+        d="M 59.06 111.26 A 8 8 90 0 1 59.06 127.26"
+        stroke="#fcd34d"
         strokeWidth="4"
+        strokeLinecap="round"
         fill="none"
-        initial={{ pathLength: 0, rotate: -90 }}
-        animate={{ pathLength: 1, rotate: -90 }}
+        initial={{ opacity: 0, pathLength: 0 }}
+        animate={{ opacity: 1, pathLength: 1 }}
+        transition={{ delay: 0.2, duration: 0.2 }}
+      />
+      <motion.path
+        d="M 59.06 111.26 A 8 8 90 0 0 59.06 127.26"
+        stroke="#fcd34d"
+        strokeWidth="4"
+        strokeLinecap="round"
+        fill="none"
+        initial={{ opacity: 0, pathLength: 0 }}
+        animate={{ opacity: 1, pathLength: 1 }}
         transition={{ delay: 0.2, duration: 0.2 }}
       />
     </svg>
